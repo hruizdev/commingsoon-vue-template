ee766c5d4696cc638f304932a5ddeeb9467be1bc
hruizdev <h.ruizcarmona@gmail.com>
2023-10-21 09:23:37 -0500

    First commit

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template>
+    <CommingSoon01/>
+</template>
+
+<script setup lang="ts">
+import CommingSoon01 from "./components/CommingSoon01.vue";
+</script>
---
+++ src/components/CommingSoon01.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="main-area center-text container">
+        <div class="display-table">
+            <div class="display-table-cell">
+                <h1 class="title font-white"><b>Comming Soon</b></h1>
+                <p class="desc font-white">Our website is currently undergoing scheduled maintenance.
+                    We Should be back shortly. Thank you for your patience.</p>
+                <a class="notify-btn" href="#"><b>NOTIFY US</b></a>
+                <ul class="social-btn font-white">
+                    <li><a href="#">facebook</a></li>
+                    <li><a href="#">twitter</a></li>
+                    <li><a href="#">google</a></li>
+                    <li><a href="#">instagram</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
+<script setup lang="ts">
+import '../assets/comming01.css'
+</script>
+<style scoped>
+
+</style>
